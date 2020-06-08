4a6f5841176552844e5d7657db463a27a35242ed
Arpitha Shankar S I <arpithashankar@github.com>
2020-06-08 06:21:57 +0530

    a

---
+++ Discount/Discount/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//// Use the decimal type to compute a discount
+namespace Discount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal price, discount, discounted_Price;
+            Console.WriteLine("enter the price and discount");
+            price = decimal.Parse(Console.ReadLine());
+            discount = decimal.Parse(Console.ReadLine());
+            discounted_Price = price - (price * discount);
+            Console.WriteLine("the discount is " + discounted_Price);
+     
+        }
+    }
+}
---
+++ Future_value/Future_value/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Future_value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal amount,rate_of_Return;
+            int i, Years;
+            amount = 1000.0M;
+            rate_of_Return = 0.07M;
+            Years = 10;
+            Console.WriteLine("Original investment: $" + amount);
+            Console.WriteLine("Rate of return: " + rate_of_Return);
+            Console.WriteLine("Over " + Years + " years");
+            for (i = 0; i < Years; i++)
+            {
+                amount = amount + (amount * rate_of_Return);
+
+            }
+            Console.WriteLine("Future value is $" + amount);
+        }
+    }
+}
